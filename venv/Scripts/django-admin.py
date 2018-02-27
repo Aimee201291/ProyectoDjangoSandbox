0b6d189685c77c3f00183c0c3a40cb4d4fd6ec6f
Aimee Rodriguez <amylinsy@gmail.com>
2018-02-27 12:42:51 -0400

    segundo commit de prueba

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\carlos\downloads\documentos_aimee\desarrollo\proyectodjango\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
